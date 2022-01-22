8bb0c488f135ed874e5cad6dae18205999c7ab3c
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2022-01-22 15:23:10 +0000

    Function for saving homeworking title, description and content

--- messenger/routes/homeworks.js
+++ messenger/routes/homeworks.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Homework = require('./../models/homework')
 const router = express.Router()
 
 router.get('/new', (req, res) => {
@@ -9,5 +10,20 @@ router.post('/', async (req, res, next) => {
     req.homework = new Homework()
     next()
 }, saveHomeworkAndRedirect('new'))
+
+function saveHomeworkAndRedirect(path) {
+    return async (req, res) => {
+        let homework = req.homework
+            homework.title = req.body.title,
+            homework.description = req.body.description,
+            homework.content = req.body.content
+        try {
+            homework = await homework.save()
+            res.redirect(`/homeworks/${homework.slug}`)
+        } catch (e) {
+            res.render(`homeworks/${path}`, { homework: homework })
+        }
+    }
+}
 
 module.exports = router
--- messenger/server.js
+++ messenger/server.js
@@ -8,13 +8,13 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-
 const mongoose = require('mongoose')
 const homeworkRouter = require('./routes/homeworks')
 
 mongoose.connect('mongodb://localhost/learnt')
 
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', async (req, res) => {
     const homeworks = await Homework.find().sort({ createdAt: 'desc' })
