0fa2a0878ce7ad5faa965217053ed194ccccf587
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2022-01-22 14:34:13 +0000

    router set up to create new homework

--- messenger/routes/homeworks.js
+++ messenger/routes/homeworks.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+router.get('/new', (req, res) => {
+    res.render('homeworks/new', { homework: new Homework() })
+})
 
 module.exports = router
